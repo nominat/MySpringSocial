1eb6149a4bcb4645b8db51e3f63643725fb767dc
inominat <ilya.nominat@t-systems.com>
2019-02-08 15:11:25 +0300

    Initial commit

---
+++ src/main/java/com/example/social/Application.java
@@ -0,0 +1,16 @@
+package com.example.social;
+
+/**
+ * Created by inominat on 08.02.2019.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
